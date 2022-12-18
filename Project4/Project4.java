05a3e4747cba2f4a8b14095c2f1180af15d843b0
ShigumaH <120991924+ShigumaH@users.noreply.github.com>
2022-12-18 18:28:59 -0500

    Create Project4.java

---
+++ Project4/Project4.java
@@ -0,0 +1,12 @@
+/**
+ * A program to display the line numbers words starting with vowels appear in.
+ * A text file is chosen by the user and then read in.
+ * 
+ */
+public class Project4 {
+    static WordGUI wordGUI;
+    
+    public static void main(String[] args) {
+        wordGUI = new WordGUI();
+    }
+}
