db7d36452e79b1aba94607882d87e80792c35855
ShigumaH <120991924+ShigumaH@users.noreply.github.com>
2022-12-18 18:26:56 -0500

    Create Project3.java

---
+++ Project3/Project3.java
@@ -0,0 +1,12 @@
+/**
+ * A program that can open a file and display its contents,
+ * and also display words starting with vowels in sorted order.
+ * 
+ */
+public class Project3 {
+    static WordGUI wordGUI;
+    
+    public static void main(String[] args) {
+        wordGUI = new WordGUI();
+    }
+}
